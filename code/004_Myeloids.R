67ec18c831ee3b60dfeffff80a8075feee8cc113
Wangjien <52444578+Wangjien@users.noreply.github.com>
2024-05-28 18:51:05 +0800

    new

--- code/004_Myeloids.R
+++ code/004_Myeloids.R
@@ -123,8 +123,122 @@ scRNA@meta.data %<>% dplyr::mutate(
     )
 )
 
+# 4 绘制DimPlot*******************************************************************************************************************************
 p1 <- DimPlot(scRNA,
-  group.by = "celltype2", label = F, repel = T, raster = F,reduction = 'BBKNNUMAP2D',cols = RColorBrewer::brewer.pal(n = 12, name = "Paired")) +
+  group.by = "celltype2", label = T, repel = T, raster = F,reduction = 'BBKNNUMAP2D',cols = RColorBrewer::brewer.pal(n = 12, name = "Paired")) +
   guides(color = guide_legend(override.aes = list(size = 6))) +
   theme(plot.title = element_blank()) + labs(title = "") +
   theme_blank(xlab = "UMAP1", ylab = "UMAP2")+ggtitle(paste0('nCells:',dim(scRNA)[2]))
+ggsave(filename = "./002_髓系大群DimPlot.png", height = 4, width = 6, plot = p1, bg = "white")
+ggsave(filename = "./002_髓系大群DimPlot.pdf", height = 4, width = 6, plot = p1, bg = "white", family = "ArialMT")
+
+p2 <- DimPlot(scRNA,
+  group.by = "sample", label = F, repel = T, raster = F,reduction = 'BBKNNUMAP2D',
+  cols = c(RColorBrewer::brewer.pal(n = 12, name = "Paired"), 
+  RColorBrewer::brewer.pal(n = 8, name = "Set2"),RColorBrewer::brewer.pal(n = 6, name = "Dark2"))) +
+  guides(color = guide_legend(override.aes = list(size = 6))) +
+  theme(plot.title = element_blank()) + labs(title = "") +
+  theme_blank(xlab = "UMAP1", ylab = "UMAP2")+ggtitle('Patient')
+ggsave(filename = "./002_髓系大群DimPlotSample.png", height = 4, width = 6, plot = p2, bg = "white")
+ggsave(filename = "./002_髓系大群DimPlotSample.pdf", height = 4, width = 6, plot = p2, bg = "white", family = "ArialMT")
+
+p3 <- DimPlot(scRNA, group.by = "group1",reduction = 'BBKNNUMAP2D',label = F,repel = T,cols = c(RColorBrewer::brewer.pal(n = 12, name = "Paired"), 
+  RColorBrewer::brewer.pal(n = 8, name = "Set2"),RColorBrewer::brewer.pal(n = 6, name = "Dark2"))) +
+  guides(color = guide_legend(override.aes = list(size = 6))) +
+  theme_blank(xlab = "UMAP1", ylab = "UMAP2")+ labs(title ="Group") +
+  theme(plot.title = element_text(hjust = 0.5,size = 18,color="black"), legend.text = element_text(size = 10, color = "black"),
+  legend.position = "right")
+ggsave(filename = "./002_髓系大群DimPlotGroup.png", height = 4, width = 5, plot = p3, bg = "white")
+ggsave(filename = "./002_髓系大群DimPlotGroup.pdf", height = 4, width = 5, plot = p3, bg = "white", family = "ArialMT")
+
+p4 <- DimPlot(scRNA,group.by = "celltype2", label = F, repel = T, raster = F,split.by = 'group1',ncol = 6,reduction = 'BBKNNUMAP2D',
+  cols = RColorBrewer::brewer.pal(n = 12, name = "Paired")) +
+  guides(color = guide_legend(override.aes = list(size = 6))) +
+  theme(plot.title = element_blank()) + labs(title = "") +
+  theme_blank(xlab = "UMAP1", ylab = "UMAP2")
+p4 <- p4 + theme(strip.text = element_text(size = 20, color = "black", face = "bold"),strip.background = element_rect(fill = '#fffbe9'))
+ggsave(filename = "./002_髓系大群SplitDimPlot06.png", height = 4, width = 26, plot = p4, bg = "white",limitsize = FALSE)
+ggsave(filename = "./002_髓系大群SplitDimPlot06.pdf", height = 4, width = 26, plot = p4, bg = "white", family = "ArialMT",limitsize = FALSE)
+
+mark_list <- list(
+    Macrophage = c("C1QA", "C1QB"),
+    Monocyte = c("VCAN", "FCN1"),
+    Neutrophils = c("FCGR3B", "CXCR2"),
+    Basophils = c("CLC", "GATA2"),
+    pDC = c("GZMB", "SCT"),
+    DC1 = c("CLEC9A", "XCR1"),
+    DC2 = c("CD1C", "CD1E"),
+    DC3 = c("HMSD", "ANKRD33B"),
+    Mast = c("CPA3", "TPSAB1")
+)
+plist <- list()
+for (i in 1:length(mark_list)) {
+    for (j in mark_list[[i]]) {
+        plist[[paste0(i, "|", j)]] <-
+            FeaturePlot(scRNA,
+                features = j, reduction = "BBKNNUMAP2D", pt.size = 0.0001, max.cutoff = 1.5,
+                cols = c("#FFEFD5", "#E6E6FA", "#87CEFA", "#6495ED", "#4169E1", "#0000CD", "#000080")
+            ) +
+            scale_x_continuous("") + scale_y_continuous("") +
+            theme_bw() +
+            theme(
+                panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                axis.ticks = element_blank(), axis.text = element_blank(),
+                legend.position = "none",
+                plot.title = element_text(hjust = 0.5, size = 18)
+            ) + ggtitle(paste0(names(mark_list[i]), " (", j, ")"))
+    }
+}
+ggsave(filename = "./002_髓系大群FeaturePlot.png", height = 9, width = 18,plot = patchwork::wrap_plots(plist, ncol = 6, nrow = 3))
+ggsave(filename = "./002_髓系大群FeaturePlot.pdf", height = 9, width = 18,plot = patchwork::wrap_plots(plist, ncol = 6, nrow = 3))
+
+message(paste0(Sys.time()," 绘制marker密度热图..."))
+reduction <- 'BBKNNUMAP2D'
+plist <- list()
+for(i in names(mark_list)){
+  for(j in mark_list[[i]]){
+    result <- tryCatch({
+       scCustomize::Plot_Density_Custom(scRNA, features = j,reduction = reduction, viridis_palette = "viridis")+
+        labs(title = paste0(i,":",j))+
+        Seurat::NoGrid()+
+        Seurat::NoAxes()+
+        theme_test(base_size = 15,
+       base_line_size = 1.3,
+       base_rect_size = 1.3)+
+    theme(plot.title = element_text(hjust =0.5),axis.text =element_blank(),axis.ticks = element_blank(), axis.title = element_blank(),legend.title = element_text(size = 15), legend.text =element_text(size =12))
+    },error = function(e){
+      message(paste0(i,"|",j," error!"))
+    },finally={
+      message(paste0(Sys.time()," ",i,"|",j, " finish..."))
+    })
+    plist[[paste0(i,'|',j)]] <- result
+  }
+}
+ggsave(filename = "./002_髓系大群基因表达密度图.png", height = 9, width = 21,plot = patchwork::wrap_plots(plist, ncol = 6, nrow = 3))
+ggsave(filename = "./002_髓系大群基因表达密度图.pdf", height = 9, width = 21,plot = patchwork::wrap_plots(plist, ncol = 6, nrow = 3))
+# 5 绘制比例变化箱线图***************************************************************************************************************************
+
+
+
+
+
+
+
+# 7 提取出其中的Macrophage和Monocytes再次聚类分群*************************************************************************************************
+scRNA <- scRNA[,scRNA$celltype2 %in% c('Macrophage','Monocyte')]
+scRNA <- CreateSeuratObject(scRNA@assays$RNA@counts)
+scRNA$sample <- stringr::str_split_fixed(rownames(scRNA@meta.data), "_[A|T|G|C].*", n = 2)[, 1]
+scRNA <- SCP::Integration_SCP(scRNA, batch = "sample", integration_method = "Harmony", cluster_resolution = seq(0.1, 1.5, 0.1))
+qsave(scRNA, file = "./06_髓系数据MacroMono_scp.qs")
+# BBKNN聚类
+scRNA <- SCP::Integration_SCP(scRNA, batch = "sample", integration_method = "BBKNN", cluster_resolution = seq(0.1, 1.5, 0.1))
+qsave(scRNA, file = "./06_髓系数据MacroMono_scp.qs")
+# scVI聚类
+scRNA <- SCP::Integration_SCP(scRNA, batch = "sample", integration_method = "scVI", cluster_resolution = seq(0.1, 1.5, 0.1))
+qsave(scRNA, file = "./06_髓系数据MacroMono_scp.qs")
+# # Seurat聚类
+# scRNA <- SCP::Integration_SCP(scRNA, batch = "sample", integration_method = "Seurat", cluster_resolution = seq(0.1, 1.5, 0.1))
+# qsave(scRNA, file = "./06_髓系数据_scp.qs")
+# ComBat聚类
+scRNA <- SCP::Integration_SCP(scRNA, batch = "sample", integration_method = "ComBat", cluster_resolution = seq(0.1, 1.5, 0.1))
+qsave(scRNA, file = "./06_髓系数据MacroMNO_scp.qs")
